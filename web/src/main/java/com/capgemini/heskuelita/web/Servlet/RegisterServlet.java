6a89cc12162db21eb6cd419bc4425021f899de67
Marcos Inca <marcosincacorreo@hotmail.com>
2019-03-12 00:00:24 -0300

    03-11-19

--- web/src/main/java/com/capgemini/heskuelita/web/Servlet/RegisterServlet.java
+++ web/src/main/java/com/capgemini/heskuelita/web/Servlet/RegisterServlet.java
@@ -1,52 +1,33 @@
 package com.capgemini.heskuelita.web.Servlet;
 
-import com.capgemini.heskuelita.core.beans.User;
+
 import com.capgemini.heskuelita.data.db.DBConnectionManager;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import java.io.*;
-import java.sql.PreparedStatement;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
 
-@WebServlet ("/register")
+@WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
 
-    public RegisterServlet () {
-        super ();
-    }
+
 
     @Override
-    public void init (ServletConfig config){
+    public void init (ServletConfig config) throws ServletException {
 
         ServletContext context = config.getServletContext();
 
         DBConnectionManager manager = (DBConnectionManager) context.getAttribute("db");
+
+        try {
+
+            this.securityService = new StudentSecurityServiceImpl(new StudentDaoJDBC(manager.getConnection()));
+        } catch (Exception e) {
+
+            throw new ServletException(e);
+        }
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
-
-        User user = new User ();
-
-        this.insertTable("user",
-                user.setUserName (req.getParameter("username")),
-                user.setPassword (req.getParameter("pwd")),
-                user.setPassword (req.getParameter("email")));
-
-        resp.sendRedirect("index.jsp");
-
-    }
-
-    public void insertTable (String username, String pwd, String email) {
-
-
-        PreparedStatement pstm = manager.getConnection ().prepareStatement ("INSERT INTO users VALUES (?, ?, ?)");
-
-        pstm.setString (1, username);
-        pstm.setString (2, pwd);
-        pstm.setString (3, email);
-
-        pstm.executeUpdate ();
-    }
 }
